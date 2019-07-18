4491c8a981d69ed0c61741771fef50dafc180f1a
Troy Moench <brewersarenumber1@gmail.com>
2019-07-17 21:35:09 -0500

    Added command line arg for the order file

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,20 @@
 extern crate seawarfare_rs;
 use seawarfare_rs::sim_manager::*;
-
+use std::env;
 extern crate chrono;
 
 
 fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 2 {
+        println!("Usage: {} <file>", args[0]);
+        std::process::exit(1);
+    }
+    let filename = &args[1];
+
     let mut sim = SimManager::new();
-    sim.init("orders01.txt");
+    sim.init(filename);
     sim.print();
     sim.print_navy();
     sim.print_orders();
